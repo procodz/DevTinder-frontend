84f2567f41838d961e2cc24fb18801cb5f849e43
raghvendra <raghu0666kumar@gmail.com>
2024-10-14 10:31:26 +0530

    calling backend api using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import Footer from './Footer'
 import Login from './Login'
 import Body from './Body'
@@ -17,7 +16,6 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-    <h1 className="text-3xl font-bold ">DevTinder front end</h1>
     <Footer />
       
     </>
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 function Login() {
+  const [emailId, setEmailId] = useState("elon@gmail.com");
+  const [password, setPassword] = useState("Elon@1234");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:3000/login", 
+      {
+      emailId, password,
+    },
+    {withCredentials: true}
+  );
+  }catch(err){
+      console.error(err);
+    }
+  }
   return (
-    <div>Login Page</div>
+    <div className="flex justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs py-3">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={(e) => setEmailId(e.target.value)} />
+            </label>
+          </div>
+          <div>
+            <label className="form-control w-full max-w-xs py-3">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+          </div>
+          <div className="card-actions justify-center py-4">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div></div>
   )
 }
 
