2aba7f2760de4933b732b43905d514e72b431d18
raghvendra <raghu0666kumar@gmail.com>
2025-01-19 23:11:55 +0530

    fixe bug of EditProfile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import EditProfile from './EditProfile'
+// import React from 'react'
+// import EditProfile from './EditProfile'
+// import { useSelector } from 'react-redux';
+// import UserCard from './UserCard';
+
+// const Profile = () => {
+//   const user = useSelector((store) => store.user);
+//   // console.log("is user array",Array.isArray(user));
+//   return (
+//     user &&(
+//     <EditProfile user = {user.user} />
+//   )
+  
+//   )
+// };
+
+
+import React, { useState } from 'react';
+import EditProfile from './EditProfile';
 import { useSelector } from 'react-redux';
+import UserCard from './UserCard';
 
 const Profile = () => {
   const user = useSelector((store) => store.user);
-  // console.log("is user array",Array.isArray(user));
+  // Initialize localUser state only when user data is available
+  const [localUser, setLocalUser] = useState(user ? user[0] : {});
+
+  // Function to handle form changes
+  const handleFormChange = (updatedUser) => {
+    setLocalUser(updatedUser);
+  };
+
+  // Only render if user exists and has data
   return (
-    user &&(
-    <EditProfile user = {user.user} />
-  )
-  )
+    user && user.length > 0 && (
+      <div className="flex justify-around items-start">
+        <EditProfile 
+          user={user[0]} 
+          onFormChange={handleFormChange}
+        />
+        <div className="mt-10">
+          <UserCard user={localUser} />
+        </div>
+      </div>
+    )
+  );
 };
 
-export default Profile
+export default Profile;
